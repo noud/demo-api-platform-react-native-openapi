a8311337019507e0a612da5fd18848a44262cf7e
Noud de Brouwer <noud4@home.nl>
2021-01-07 16:25:38 +0100

    cleanup

--- sbin/provision.sh
+++ sbin/provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+
 APP_NAME=my-app
-# bin/app-name.sh
+# sbin/app-name.sh
 
 sudo rm -r ${APP_NAME}
 rm -r node_modules package-lock.json yarn.lock
@@ -13,7 +14,6 @@ cd ${APP_NAME}
 yarn add redux react-redux redux-thunk redux-form react-router-dom connected-react-router prop-types
 yarn add --dev @api-platform/client-generator
 yarn add react-native-elements@0.19.0 react-native-router-flux react-native-vector-icons whatwg-url buffer react-native-event-source react-native-gesture-handler react-native-reanimated react-native-screens
-yarn add util # for whatwg-url
 
 cd .. && sbin/generate.sh
 cd ${APP_NAME}
--- src-edited/config/entrypoint.js
+++
@@ -1,3 +0,0 @@
-export const ENTRYPOINT = 'https://demo.api-platform.com';
-// export const ENTRYPOINT = 'http://localhost:8080/';
-// export const ENTRYPOINT = 'http://api-platform-demo.localhost/';
